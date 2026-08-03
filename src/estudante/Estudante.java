c506b2616fde17a2176bab7dab4acc4ce85cf774
EduardoTedesco <eduardo.tedesco19@gmail.com>
2026-08-03 16:23:54 -0300

    inicio do estudante.java

---
+++ src/estudante/Estudante.java
@@ -0,0 +1,13 @@
+package estudante;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estudante {
+    private String nome;
+    private List<Double> notas;
+
+    public Estudante(String nome){
+        this.nome = nome;
+        this.notas = new ArrayList<>();
+    }
+}
