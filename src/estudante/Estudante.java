a92507550376b81443f911de0d85d37c171453f0
EduardoTedesco <eduardo.tedesco19@gmail.com>
2026-08-03 16:32:39 -0300

    metodo para inserir as notas

--- src/estudante/Estudante.java
+++ src/estudante/Estudante.java
@@ -1,13 +1,25 @@
 package estudante;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Estudante {
     private String nome;
     private List<Double> notas;
+    Scanner scanner = new Scanner(System.in);
 
     public Estudante(String nome){
         this.nome = nome;
         this.notas = new ArrayList<>();
     }
+
+    public void insereNotas(){
+        for(Integer i=0; i <= 5; i++){
+            System.out.println("Digite a " + i+1 + "nota do aluno " + this.nome + ":");
+            Double nota = scanner.nextDouble();
+            this.notas.add(nota);
+        }
+    }
+
+    
 }
